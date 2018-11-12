b07250914441cefafd1aa36af1d6ef8dfa978c19
一心 <ulquiola@163.com>
2018-11-12 11:55:10 +0800

    1

--- 增删改查/App_Start/RouteConfig.cs
+++ 增删改查/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace 增删改查
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "UserInfo", action = "UserInfo", id = UrlParameter.Optional }
             );
         }
     }
---
+++ 增删改查/Controllers/UserInfoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using 增删改查.Models;
+
+namespace 增删改查.Controllers
+{    
+    public class UserInfoController : Controller
+    {
+        testEntities context = new Models.testEntities();
+        // GET: UserInfo
+        public ActionResult UserInfo()
+        {            
+            var userinfo = context.UserInfo;
+            return View(userinfo);
+        }
+        public ActionResult Update(int id)
+        {
+            //UserInfo userinfo = context.UserInfo.Find(id);
+            UserInfo userinfo = context.UserInfo.Where(n => n.UID == id).FirstOrDefault();
+            return View(userinfo);
+        }
+        [HttpPost]
+        public ActionResult Update(UserInfo userinfo)
+        {
+            int x = userinfo.UID;
+            string y = userinfo.UName;
+            string z = userinfo.UPassWord;
+            context.UserInfo.Attach(userinfo);
+            context.Entry(userinfo).State = EntityState.Modified;
+            int result = context.SaveChanges();
+            if(result>0)
+            {
+                return RedirectToAction("UserInfo");
+            }
+            else
+            {
+               return RedirectToAction("Edit", "UseInfo", new { id =userinfo.UID}); 
+            }           
+            //UserInfo user = context.UserInfo.Where(n => n.UID == userinfo.UID).First();
+            //user.UName = userinfo.UName;
+            //user.UPassWord = userinfo.UPassWord;
+            //context.SaveChanges();
+            //return RedirectToAction("UserInfo");            
+        }
+        public ActionResult Delete(int id)
+        {
+            UserInfo userinfo = context.UserInfo.Find(id);
+            context.UserInfo.Remove(userinfo);
+            context.SaveChanges();
+            return RedirectToAction("UserInfo");           
+        }
+        public ActionResult Add()
+        {
+            UserInfo userinfo = new UserInfo();
+            return View(userinfo);
+        }
+        [HttpPost]
+        public ActionResult Add(UserInfo userinfo)
+        {
+            context.UserInfo.Add(userinfo);
+            context.SaveChanges();
+            return RedirectToAction("UserInfo");
+        }
+    }
+}
